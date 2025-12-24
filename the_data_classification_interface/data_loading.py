340457612413ac946156fab3b2cdc552a0661a54
youssef <majdoubyoussef79@gmail.com>
2025-12-24 18:54:08 +0100

    this our datapipline: the class DataProvider is the function that will : -create the data set -update the data set when we add the conotation -be the middle man between the UI and the  data set -repair the data set if it get corrupted -append the data set if more data is available

--- the_data_classification_interface/data_loading.py
+++ the_data_classification_interface/data_loading.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import numpy as np
 import os,ast
 
 conversations_path="./raw_data/archive/movie_conversations.tsv"
 lines_path="./raw_data/archive/movie_lines.tsv"
-class data_provider():
+class DataProvider:
     def __init__(self):
         if(os.path.exists("./data_set/data_stat.tsv")):
            self.data_stat=pd.read_csv("./data_set/data_stat.tsv",sep="\t")
@@ -11,8 +12,9 @@ class data_provider():
             self.create_data_set()
     def create_data_set(self):
         conversations=pd.read_csv(conversations_path,sep="\t",header=None,names=["per1","per2","movie_id","utterance_ids"])
-        lines=pd.read_csv(lines_path,sep="\t",header=None,names=["line_id","character_id","movie_id","character_name","text"])
+        lines=pd.read_csv(lines_path,sep="\t",header=None,names=["line_id","character_id","movie_id","character_name","text"],index_col="line_id")
         os.makedirs("./data_set/conversations",exist_ok=True)
+        os.makedirs("./data_set/stats",exist_ok=True)
         stat_data={"id":[],"disscussion_length":[]}
         for i in range(len(conversations)):
             disscussion_df=self.create_conversation(conversations,lines,i)
@@ -29,7 +31,7 @@ class data_provider():
         lines_ids=lines_ids.strip(" ")
         lines_ids=lines_ids.replace(" ",",")
         lines_ids=ast.literal_eval(lines_ids)
-        conversation_lines=lines[lines["line_id"].isin(lines_ids)]
+        conversation_lines=lines.loc[lines_ids]
         for i in range(len(conversation_lines)):
             disscussion["id"].append(conversation_id+1)
             disscussion["lines_index"].append(i)
@@ -42,14 +44,14 @@ class data_provider():
         disscussion_stat={"id":[],"line_index":[],"connotation":[]}
         for i in range(number_of_lines):
             disscussion_stat["id"].append(conversation_id)
-            disscussion_stat["line_index"].append(i)
-            disscussion_stat["connotation"].append("None")
+            disscussion_stat["line_index"].append(i+1)
+            disscussion_stat["connotation"].append(np.nan)
         disscussion_stat_df=pd.DataFrame(disscussion_stat)
         print(disscussion_stat_df.head())
         return disscussion_stat_df
     def data_set_append(self):
         conversations=pd.read_csv(conversations_path,sep="\t",header=None,names=["per1","per2","movie_id","utterance_ids"])
-        lines=pd.read_csv(lines_path,sep="\t",header=None,names=["line_id","character_id","movie_id","character_name","text"])
+        lines=pd.read_csv(lines_path,sep="\t",header=None,names=["line_id","character_id","movie_id","character_name","text"],index_col="line_id")
         stat_data=pd.read_csv("./data_set/data_stat.tsv",sep="\t")
         starting_index=max(stat_data["id"])
         for i in range(starting_index,len(conversations)):
@@ -57,20 +59,21 @@ class data_provider():
             disscussion_df.to_csv(f"./data_set/conversations/conversation_{i+1}.tsv",sep="\t",index=False)
             disscussion_stat_df=self.create_disscussion_stat(i,len(disscussion_df))
             disscussion_stat_df.to_csv(f"./data_set/stats/stat_{i+1}.tsv",sep="\t",index=False)
-            stat_data["id"].append(i+1)
-            stat_data["disscussion_length"].append(len(disscussion_df))
-        self.data_stat=pd.DataFrame(stat_data)
+            stat_data.loc[len(stat_data)]={"id":i+1,"disscussion_length":len(disscussion_df)}
+        self.data_stat=stat_data
         self.data_stat.to_csv("./data_set/data_stat.tsv",sep="\t",index=False)
     def repair_mode_from_raw_data(self):
+        os.makedirs("./data_set/conversations",exist_ok=True)
+        os.makedirs("./data_set/stats",exist_ok=True)
         conversations=pd.read_csv(conversations_path,sep="\t",header=None,names=["per1","per2","movie_id","utterance_ids"])
-        lines=pd.read_csv(lines_path,sep="\t",header=None,names=["line_id","character_id","movie_id","character_name","text"])
+        lines=pd.read_csv(lines_path,sep="\t",header=None,names=["line_id","character_id","movie_id","character_name","text"],index_col="line_id")
         stat_data=pd.read_csv("./data_set/data_stat.tsv",sep="\t")
         if len(stat_data)<len(conversations):
             print("there is missing conversations,please wait for a full report")
         for i in range(len(conversations)):
+            disscussion_df=self.create_conversation(conversations,lines,i)
             if not os.path.exists(f"./data_set/conversations/conversation_{i+1}.tsv"):
                 print(f"conversation {i+1} is missing, recreating it")
-                disscussion_df=self.create_conversation(conversations,lines,i)
                 disscussion_df.to_csv(f"./data_set/conversations/conversation_{i+1}.tsv",sep="\t",index=False)
             if not os.path.exists(f"./data_set/stats/stat_{i+1}.tsv"):
                 print(f"stat for conversation {i+1} is missing, recreating it")
@@ -78,7 +81,7 @@ class data_provider():
                 disscussion_stat_df.to_csv(f"./data_set/stats/stat_{i+1}.tsv",sep="\t",index=False)
             if not (i+1) in stat_data["id"].values:
                 print(f"stat entry for conversation {i+1} is missing, adding it")
-                stat_data=stat_data.append({"id":i+1,"disscussion_length":len(disscussion_df)},ignore_index=True)
+                stat_data.loc[len(stat_data)]={"id":i+1,"disscussion_length":len(disscussion_df)}
         self.data_stat=pd.DataFrame(stat_data)
         self.data_stat.to_csv("./data_set/data_stat.tsv",sep="\t",index=False)
     def get_conversation(self,conversation_id,include_stat=False):
