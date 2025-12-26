6fc69cccd393cb4289ff778391e40e240a46c6d2
youssef <majdoubyoussef79@gmail.com>
2025-12-26 23:09:16 +0100

    debugging: -resolving inconsistent columns names adding: -a is_completed column to data_stat.tsv to count the  number of lines that have been labled/annotated in each disscussion

--- the_data_classification_interface/data_loading.py
+++ the_data_classification_interface/data_loading.py
@@ -64,9 +64,9 @@ class DataProvider:
         self.cleaner=DataCleaner([conversations_path,lines_path],[4,5])
         self.conversations_path,self.lines_path=self.cleaner.save_cleaned()
         stats_exists = os.path.exists("./data_set/data_stat.tsv")
-        conv_dir_exists = os.path.exists(self.conversations_path)
-        line_dir_exists = os.path.exists(self.lines_path)
-        if(stats_exists or conv_dir_exists or line_dir_exists):
+        conv_dir_exists = os.path.exists("./data_set/conversations")
+        stat_dir_exists = os.path.exists("./data_set/stats")
+        if(stats_exists or conv_dir_exists or stat_dir_exists):
             self.repair_mode_from_raw_data() 
         else:
             self.create_data_set()
@@ -76,14 +76,17 @@ class DataProvider:
                           names=["line_id","character_id","movie_id","character_name","text"],index_col="line_id")
         os.makedirs("./data_set/conversations",exist_ok=True)
         os.makedirs("./data_set/stats",exist_ok=True)
-        stat_data={"id":[],"disscussion_length":[]}
+        stat_data={"id":[],"disscussion_length":[],"is_complete":[]}
         for i in tqdm(range(len(conversations)), desc="Creating conversations"):
             disscussion_df=self.create_conversation(conversations,lines,i)
+            if(len(disscussion_df)==0):
+                print("there is a problem")
             disscussion_df.to_csv(f"./data_set/conversations/conversation_{i+1}.tsv",sep="\t",index=False)
             disscussion_stat_df=self.create_disscussion_stat(i,len(disscussion_df))
             disscussion_stat_df.to_csv(f"./data_set/stats/stat_{i+1}.tsv",sep="\t",index=False)
             stat_data["id"].append(i+1)
             stat_data["disscussion_length"].append(len(disscussion_df))
+            stat_data["is_complete"].append(0)
         self.data_stat=pd.DataFrame(stat_data)
         self.data_stat.to_csv("./data_set/data_stat.tsv",sep="\t",index=False)
     def create_conversation(self,conversations,lines,conversation_id):
@@ -101,10 +104,10 @@ class DataProvider:
         disscussion_df=pd.DataFrame(disscussion)
         return disscussion_df
     def create_disscussion_stat(self,conversation_id,number_of_lines):
-        disscussion_stat={"id":[],"line_index":[],"connotation":[]}
+        disscussion_stat={"id":[],"lines_index":[],"connotation":[]}
         for i in range(number_of_lines):
             disscussion_stat["id"].append(conversation_id+1)
-            disscussion_stat["line_index"].append(i)
+            disscussion_stat["lines_index"].append(i)
             disscussion_stat["connotation"].append(np.nan)
         disscussion_stat_df=pd.DataFrame(disscussion_stat)
         return disscussion_stat_df
@@ -131,25 +134,30 @@ class DataProvider:
         try:
             self.data_stat=pd.read_csv("./data_set/data_stat.tsv",sep="\t")
         except:
-            self.data_stat=pd.DataFrame({"id":[],"disscussion_length":[]})
-        new={"id":[],"disscussion_length":[]}
+            self.data_stat=pd.DataFrame({"id":[],"disscussion_length":[],"is_complete":[]})
+        new={"id":[],"disscussion_length":[],"is_complete":[]}
         ids=set(self.data_stat['id'].values)
         if len(self.data_stat)<len(conversations):
             print("there is missing conversations,please wait for a full report")
         for i in tqdm(range(len(conversations)), desc="Repairing dataset"):
-            stat=1
+            stat,log=1,1
             if not os.path.exists(f"./data_set/conversations/conversation_{i+1}.tsv"):
                 disscussion_df=self.create_conversation(conversations,lines,i)
                 disscussion_df.to_csv(f"./data_set/conversations/conversation_{i+1}.tsv",sep="\t",index=False)
-                stat=0
+                log=0
             if not os.path.exists(f"./data_set/stats/stat_{i+1}.tsv"):
-                disscussion_stat_df=self.create_disscussion_stat(i,len(disscussion_df))
-                disscussion_stat_df.to_csv(f"./data_set/stats/stat_{i+1}.tsv",sep="\t",index=False)
+                if log:
+                    disscussion_df=pd.read_table(f"./data_set/conversations/conversation_{i+1}.tsv")
+                stat_df=self.create_disscussion_stat(i,len(disscussion_df))
+                stat_df.to_csv(f"./data_set/stats/stat_{i+1}.tsv",sep="\t",index=False)
+                stat=0
             if not (i+1) in ids:
                 if stat:
-                    disscussion_df=self.create_conversation(conversations,lines,i)
+                    stat_df=pd.read_table(f"./data_set/stats/stat_{i+1}.tsv",header=None)
                 new['id'].append(i+1)
-                new['disscussion_length'].append(len(disscussion_df))
+                length=len(stat_df)
+                new['disscussion_length'].append(length)
+                new['is_complete'].append(length-stat_df['connotation'].isna().sum())
                 ids.add(i+1)
         self.data_stat=pd.concat([self.data_stat,pd.DataFrame(new)])
         self.data_stat.to_csv("./data_set/data_stat.tsv",sep="\t",index=False)
@@ -157,13 +165,12 @@ class DataProvider:
         disscussion_df=pd.read_csv(f"./data_set/conversations/conversation_{conversation_id}.tsv",sep="\t",)
         if include_stat:
             disscussion_stat_df=pd.read_csv(f"./data_set/stats/stat_{conversation_id}.tsv",sep="\t",)
-            disscussion_df=pd.merge(disscussion_df,disscussion_stat_df,left_on=["id","lines_index"],right_on=["id","line_index"])
+            disscussion_df=pd.merge(disscussion_df,disscussion_stat_df,left_on=["id","lines_index"],right_on=["id","lines_index"])
         return disscussion_df
     def get_disscusion_stats(self,conversation_id):
         disscussion_stat_df=pd.read_csv(f"./data_set/stats/stat_{conversation_id}.tsv",sep="\t")
         return disscussion_stat_df
     def update_connotation(self,conversation_id,line_index,connotation):
         disscussion_stat_df=pd.read_csv(f"./data_set/stats/stat_{conversation_id}.tsv",sep="\t")
-        disscussion_stat_df.loc[disscussion_stat_df["line_index"]==line_index,"connotation"]=connotation
+        disscussion_stat_df.loc[disscussion_stat_df["lines_index"]==line_index,"connotation"]=connotation
         disscussion_stat_df.to_csv(f"./data_set/stats/stat_{conversation_id}.tsv",sep="\t",index=False)
-provider=DataProvider()
